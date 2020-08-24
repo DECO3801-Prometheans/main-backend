c01ced87359a89e1e0037d55afa401df92eddcdf
ba11breaker <15212219417@163.com>
2020-08-24 12:13:25 +1000

    change mongodb connection

--- app/config.js
+++ app/config.js
@@ -1,5 +1,4 @@
 module.exports = {
     secret: 'hksjhf923he9hjdhf9ushf92ufoandj394r92hiesfdjshfk92y3',
-    //connectionStr : 'mongodb+srv://feather:feather980523@quora.7lgho.mongodb.net/<dbname>?retryWrites=true&w=majority',
     connectionStr : 'mongodb+srv://admin:TXBBzzxbNcny2UY@cluster0.0m4qa.mongodb.net/app-data?retryWrites=true&w=majority',
 };
